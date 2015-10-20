be828ec81e71e87f153ade90a359d95fb64d1601
Jonas Bohatsch <jbohatsch@gmail.com>
2015-10-20 16:04:42 +0200

    liftoff

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public Color lerpedColor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<SpriteRenderer>().color = lerpedColor;
+	}
+
+	public float speed = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 30) * Time.deltaTime * speed);
+		lerpedColor = Color.Lerp(Color.white, Color.black, speed);
+
+	}
+
+}
