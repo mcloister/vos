e5992ff4d227a95a99d400976244a714ca4d404d
Jo Bo <jbohatsch@gmail.com>
2015-10-22 17:51:57 +0200

    added shots

---
+++ Assets/Shooter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+
+	public GameObject ball;
+	public Transform ballSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(ball, ballSpawn.position, ballSpawn.rotation);
+		}
+	}
+}
